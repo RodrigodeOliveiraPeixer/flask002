95e6453162cfb7bc5d49102d9d5f1afd2f9af027
RodrigodeOliveiraPeixer <109769171+RodrigodeOliveiraPeixer@users.noreply.github.com>
2022-10-31 23:40:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ app.add_url_rule("/teste", "teste", teste)
 app.add_url_rule("/teste-2", "teste2", teste2)
 
 if __name__ == '__main__':
-    app.run(debug=True, port="3000"
+    app.run(debug=True, port="3000")
